01d97abd104b7e19a0e4b9b640bc92a03a614d01
WillKoehrsen <will@cortexintel.com>
2023-02-25 13:38:48 -0500

    Initial exercise work

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,15 @@
 // functions1.rs
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     call_me();
+}
+
+fn call_me() {
+    println("Hello, World!");
+}
+
+fn println(s: &str) {
+    // println! is a macro while println is a function
+    println!("{}", s);
 }
