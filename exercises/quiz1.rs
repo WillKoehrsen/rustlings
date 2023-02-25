062dbf819325f8a4d1d6bb5243a05a9f3502ca1f
WillKoehrsen <will@cortexintel.com>
2023-02-25 14:04:29 -0500

    Working on move_semantics

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,18 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let cost: i32;
+    if quantity > 40 {
+        cost = quantity;
+    } else {
+        cost = quantity * 2;
+    }
+    println!("{} apples cost {} rustbucks", quantity, cost);
+
+    return cost
+}
 
 // Don't modify this function!
 #[test]
